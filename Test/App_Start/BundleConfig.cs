e7502c37839d9c8346e9659b3f2759d68058745d
Kabale <vincentschandeler@gmail.com>
2014-10-11 10:04:06 +0200

    Suppression des fichiers inutiles

--- Test/App_Start/BundleConfig.cs
+++ Test/App_Start/BundleConfig.cs
@@ -5,16 +5,10 @@ namespace StorageCompany
 {
     public class BundleConfig
     {
-        // Pour plus d’informations sur le regroupement, rendez-vous sur http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Content/Scripts/jquery-{version}.js"));
-
-            // Utilisez la version de développement de Modernizr pour développer et apprendre. Puis, lorsque vous êtes
-            // prêt pour la production, utilisez l’outil de génération sur http://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Content/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Content/Scripts/bootstrap.js",
